f644248327a12bd948a0a8d276b5e4cc3aa01201
Max Bay <cwang167@my.centennialcollege.ca>
2021-04-09 11:42:45 +0800

    Week 12 - Unity and Mobile Game Development

--- Assets/_Scripts/HealthBarWorldSpaceController.cs
+++ Assets/_Scripts/HealthBarWorldSpaceController.cs
@@ -5,19 +5,24 @@ using UnityEngine;
 public class HealthBarWorldSpaceController : MonoBehaviour
 {
     // 获取玩家的相机
-    private Transform playerCamera;
+    private Transform playerCameraTransform;
+    private GameObject playerCamera;
 
     private void Start()
     {
-        playerCamera = GameObject.Find("PlayerCamera").transform;
+        playerCamera = GameObject.Find("PlayerCamera");
+        if (playerCamera)
+            playerCameraTransform = playerCamera.transform;
     }
 
     // 这里使用LateUpdate，他的意思是所有的update，如FixedUpdate和Update都执行完后，才会执行LateUpdate，只有几毫秒的滞后
     void LateUpdate()
     {
-        // 让此组件看向玩家相机
-        transform.LookAt(transform.position + playerCamera.forward);
-        //transform.LookAt(playerCamera);
-
+        if (playerCameraTransform)
+        {
+            // 让此组件看向玩家相机
+            transform.LookAt(transform.position + playerCameraTransform.forward);
+            //transform.LookAt(playerCamera);
+        }
     }
 }
